7f040f3644cdb323baec12b2ab559e50bc7a5312
alessiaBer <alexdeyn@gmail.com>
2023-05-16 16:05:50 +0200

    [add] class Movie, class Genre and 2 instances

--- index.php
+++ index.php
@@ -14,7 +14,31 @@ mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi i
 organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati.
 Bonus 2 (opzionale)
 Modificare la classe Movie in modo che accetti piú di un genere.*/
+class Genre {
+    public $name;
+    public function __construct(String $name) {
+        $this->name = $name;
+    }
+};
+
+class Movie {
+    public $title;
+    public $language;
+    public $genre;
+    public $type = "movie";
+    function __construct(String $title, String $language, Genre $genre) {
+        $this->title = $title;
+        $this->language = $language;
+        $this->genre = $genre;
+    }
+};
+
+$animeGenre = new Genre('Anime');
+$thrillerGenre = new Genre('Thriller');
 
 
+$spiritedAway = new Movie('Spirited Away', 'Japanese', $animeGenre);
+$theNinthGate = new Movie('The Ninth Gate', 'English', $thrillerGenre);
 
+var_dump($spiritedAway, $theNinthGate);
 ?>
