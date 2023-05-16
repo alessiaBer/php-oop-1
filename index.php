66485dc52da4edc9870859ad1d5ffda2c799d742
alessiaBer <alexdeyn@gmail.com>
2023-05-16 16:23:50 +0200

    [add] isRatedR method

--- index.php
+++ index.php
@@ -25,11 +25,17 @@ class Movie {
     public $title;
     public $language;
     public $genre;
+    public $isRatedR = false;
     public $type = "movie";
     function __construct(String $title, String $language, Genre $genre) {
         $this->title = $title;
         $this->language = $language;
         $this->genre = $genre;
+    }
+    function isRatedR($minAge) {
+        if ($minAge >= 18) {
+            $this->isRatedR = true;
+        } 
     }
 };
 
@@ -38,7 +44,27 @@ $thrillerGenre = new Genre('Thriller');
 
 
 $spiritedAway = new Movie('Spirited Away', 'Japanese', $animeGenre);
+
 $theNinthGate = new Movie('The Ninth Gate', 'English', $thrillerGenre);
+$theNinthGate->isRatedR(20);
 
 var_dump($spiritedAway, $theNinthGate);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>My OOP Movies</h1>
+
+
+    </div>
+</body>
+</html>
